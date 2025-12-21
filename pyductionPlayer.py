16b4398f4e7889aeffb99c52bc26a1a1e9eecd50
Shamwarizim <shamwarizim9868@gmail.com>
2025-12-21 11:45:29 +1000

    cleaned up debug stuff and formatting in pyduction player

--- pyductionPlayer.py
+++ pyductionPlayer.py
@@ -1,20 +1,13 @@
-import random
-
 from functions.game import Wordle
 from functions.solver import Pyduction
 
 wordle = Wordle()
-wordle.outputAttr()
 pyduction = Pyduction(wordle)
 
-#pyduction.guess()
 
-
-##
-print('GUESS COLOUR | Score | Words w/ Identical Score')
 # Play the game.
+print('GUESS COLOUR | Score | Words w/ Identical Score')
 for i in range(6):
-    
     pyduction.guess()
     print(f'{wordle.guesses[-1]} {wordle.colouredGuesses[-1]} | S {pyduction.maxScore} | {len(pyduction.maxWords)}')
     
